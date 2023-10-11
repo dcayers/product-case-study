55614748170788cd22b50263b1879bcb240d82b0
Dewaun Ayers <dewaun.ayers@gmail.com>
2023-10-11 16:58:30 +0000

    test: lemme see somethin...

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -7,5 +7,5 @@ declare global {
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
-
+console.log('PRISMA...', prisma)
 export default prisma;
