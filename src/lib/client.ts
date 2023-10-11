ea7dbb5bb4f498ba4634b16e84b1eb1aea44a6dc
Dewaun Ayers <dewaun.ayers@gmail.com>
2023-10-11 16:44:07 +0000

    fix: it was the prisma client all along...

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from '@prisma/client'
-
-let prisma: PrismaClient;
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient;
 }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
-export default prisma
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+export default prisma;
