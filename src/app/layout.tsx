b9557420f75cac9cea235774fdebe9531e8623e2
Dewaun Ayers <dewaun.ayers@gmail.com>
2023-10-11 18:40:17 +0000

    fix: html, try data proxy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,19 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.variable}>{children}</body>
+    </html>
+  );
 }
