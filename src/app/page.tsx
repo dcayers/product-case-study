065d320af6b8671c6c8775f692259a2ae990ea90
Dewaun Ayers <dewaun.ayers@gmail.com>
2023-10-12 15:49:14 +0000

    fix: try to check vercel logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link';
-import { Flex, Button, NavLink } from "@mantine/core";
+import Link from "next/link";
+import { Flex, Button } from "@mantine/core";
 import { IconPhoto } from "@tabler/icons-react";
 import { OrdersTable } from "@/components/OrdersTable";
 import { getClient } from "@/lib/apolloClient";
 import { ORDERS_QUERY } from "@/graphql/queries";
+
+console.log(process.env.NEXT_PUBLIC_URL);
 
 export default async function Page() {
   const { data } = await getClient().query({ query: ORDERS_QUERY });
@@ -11,7 +13,13 @@ export default async function Page() {
     <Flex direction="column" gap="md">
       <Flex align="center" justify="space-between">
         <h2>Orders</h2>
-        <Button variant="light" leftSection={<IconPhoto size={14} />} component={Link} href="new" prefetch={false}>
+        <Button
+          variant="light"
+          leftSection={<IconPhoto size={14} />}
+          component={Link}
+          href="new"
+          prefetch={false}
+        >
           New Order
         </Button>
       </Flex>
