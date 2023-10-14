1437a3a5e0b848837449982bf8809021b7de7157
Dewaun Ayers <dewaun.ayers@gmail.com>
2023-10-14 11:56:30 +1100

    feat: 💅✨

--- src/app/@order/(.)order/new/page.tsx
+++ src/app/@order/(.)order/new/page.tsx
@@ -14,6 +14,7 @@ export default function NewOrder() {
   useEffect(() => {
     console.log(window);
     createDraftOrder();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- src/graphql/types/Shipping.ts
+++ src/graphql/types/Shipping.ts
@@ -3,11 +3,11 @@ import { builder } from "../builder";
 builder.prismaObject("ShippingInfo", {
   fields: (t) => ({
     id: t.exposeID("id"),
-    trackingCompany: t.exposeString("trackingCompany"),
-    trackingNumber: t.exposeString("trackingNumber"),
-    deliveryAddress: t.exposeString("deliveryAddress"),
-    contactName: t.exposeString("contactName"),
-    contactNumber: t.exposeString("contactNumber"),
-    contactEmail: t.exposeString("contactEmail"),
+    trackingCompany: t.exposeString("trackingCompany", { nullable: true }),
+    trackingNumber: t.exposeString("trackingNumber", { nullable: true }),
+    deliveryAddress: t.exposeString("deliveryAddress", { nullable: true }),
+    contactName: t.exposeString("contactName", { nullable: true }),
+    contactNumber: t.exposeString("contactNumber", { nullable: true }),
+    contactEmail: t.exposeString("contactEmail", { nullable: true }),
   }),
 });
